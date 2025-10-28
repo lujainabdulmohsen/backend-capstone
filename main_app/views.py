3610449a29ee11a0b71c8cb733ffc2f9abb19128
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2025-10-28 10:50:09 +0300

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,7 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class Home(APIView):
+    def get(self, request):
+        content = {'message': 'Welcome to the cat-collector api home route!'}
+        return Response(content)
