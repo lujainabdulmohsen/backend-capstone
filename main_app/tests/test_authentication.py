24fd05b6622375d7abd59a407c44f2b8eb063a43
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2025-11-03 09:21:44 +0300

    auth tests

---
+++ main_app/tests/test_authentication.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class AuthenticationTests(APITestCase):
+    def setUp(self):
+        """Set up test client and create test user"""
+        self.client = APIClient()
+        self.test_user = User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpass123'
+        )
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.token_refresh_url = reverse('token_refresh')
